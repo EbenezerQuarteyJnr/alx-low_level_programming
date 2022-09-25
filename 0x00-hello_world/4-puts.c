2b68b3eb926438c215b64f3f1346a9543d2536e6
EbenezerQuarteyJnr <Ebenezer998@gmail.com>
2022-09-25 12:27:28 -0700

    tadsk 4 modifications

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include < stdio.h >
+#include <stdio.h>
 / **
 * Description : main - Prints out a string to stdout .
 * Return : 0 if success.
@@ -7,5 +7,5 @@ int main(void)
     {
     puts("\"Programming is like building a multilingual puzzle"
    );
-return ( 0 );
+return (0);
     }
